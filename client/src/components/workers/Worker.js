f700d3ef708d3fe3e9ef7929f6943b724b094392
Sean Klindt <sk.klindt@gmail.com>
2021-09-29 18:03:01 -0600

    starting services

--- client/src/components/workers/Worker.js
+++ client/src/components/workers/Worker.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import WorkerForm from './WorkerForm';
+import Services from '../services/Services';
 
-
-const Worker = ({ id, name, phone }) => {
+const Worker = ({ id, name, phone, deleteWorker, updateWorker }) => {
+  const [editing, setEdit] = useState(false)
 
   return (
     <>
       <li>
-        <h2>{name}</h2>
-        <h3>{phone}</h3>
+        {name}
+        <br />
+        {phone}
+        {
+          editing ?
+          <>
+            <WorkerForm
+              id={id}
+              name={name}
+              phone={phone}
+              updateWorker={updateWorker}
+              setEdit={setEdit}
+            />
+          </>
+          :
+          <button onClick={() => setEdit(true)}>Edit</button>
+        }
+        <button onClick={() => deleteWorker(id)}>Delete</button>
       </li>
+      <Services workerId={id}/>
     </>
   )
 }
